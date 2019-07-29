4cb67fe1fbfe9318b9974311cb0485d1113a3c56
Jack Antico <jantico@intralinks.com>
2019-07-29 08:47:43 -0400

    I think this part is too long so I will stop doing it

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import './Blog.css';
 import Posts from '../Blog/Posts/Posts.js'
 import NewPost from '../Blog/NewPost/NewPost.js'
 import '../Blog/Posts/Posts.css'
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 class Blog extends Component {
     render () {
@@ -12,12 +12,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: "orange",
+                                    textDecoration: "underline"
+                                }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: 'submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
